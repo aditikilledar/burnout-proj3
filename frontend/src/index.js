03119d65cf685455c6e87b924146fbac97516fb8
shashank-madan <shashank.um.xl@gmail.com>
2023-11-17 10:24:17 -0500

    Added google sign in to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <GoogleOAuthProvider clientId="1043042736362-18f0m1ea08eip676hgveseq6luqv73rj.apps.googleusercontent.com">
+        <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
+    </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
